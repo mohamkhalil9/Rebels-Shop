94621a1a07692042a9afbf0ee1d17561771a3147
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-15 10:09:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Wolf Community`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Mixwell Community`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
